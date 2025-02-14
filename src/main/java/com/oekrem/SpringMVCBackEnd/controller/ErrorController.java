a56732cbc37d433116eb295c5252d635bd81c402
Ekrem <oekremyildirim@outlook.com>
2025-02-14 22:08:41 +0300

    Genel Error kontrolü için ErrorController ve ApiErrorResponse eklendi

--- src/main/java/com/oekrem/SpringMVCBackEnd/controller/ErrorController.java
+++ src/main/java/com/oekrem/SpringMVCBackEnd/controller/ErrorController.java
@@ -1,4 +1,36 @@
 package com.oekrem.SpringMVCBackEnd.controller;
 
+import com.oekrem.SpringMVCBackEnd.dto.Response.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex){
+        log.error(ex.getMessage(), ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .message("Exception : " + ex.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error(ex.getMessage(), ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .message("Illegal Argument Exception - BAD Request: " + ex.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
 }
--- src/main/java/com/oekrem/SpringMVCBackEnd/dto/Response/ApiErrorResponse.java
+++ src/main/java/com/oekrem/SpringMVCBackEnd/dto/Response/ApiErrorResponse.java
@@ -1,4 +1,17 @@
 package com.oekrem.SpringMVCBackEnd.dto.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiErrorResponse {
+
+    String message;
+    int status;
+
 }
